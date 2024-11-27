7cc0809ee7de38978ab98b01491c8bfc27acdbe1
joaoroccella <roccella@gmail.com>
2024-11-27 13:37:32 -0300

    feat: finalização e teste database

--- model/database.py
+++ model/database.py
@@ -1,12 +1,15 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+from os import getenv
 
 class Database:
     def __init__(self):
-        self.host = 'localhost'
-        self.user = 'root'
-        self.password = 'admin'
-        self.database = 'vingadores'
+        load_dotenv() # carregando as variáveis de ambiente
+        self.host = getenv('BD_HOST')
+        self.user = getenv('BD_USER')
+        self.password = getenv('BD_PSWD')
+        self.database = getenv('BD_DATABASE')
 
     def connect(self):
         try:
@@ -21,6 +24,16 @@ class Database:
         except Error as e:
             print(f'Erro: {e}')
 
-Database().connect()
-# db = Database()
-# db.connect()
+    def disconnect(self):
+        self.connection.close()
+        print('Conexão com o banco de dados encerrada com sucesso')
+
+    def execute_query(self, query, values=None):
+        try:
+            self.cursor.execute(query, values)
+            self.connection.commit()
+            print('Query executada com sucesso')
+            return self.cursor
+        except Error as e:
+            print(f'Erro: {e}')
+            return None
